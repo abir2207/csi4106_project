ee7c145bee925086e7a75d9107e267d3f7802638
abir2207 <71342607+abir2207@users.noreply.github.com>
2022-04-19 09:11:48 -0400

    Add files via upload

---
+++ ocr.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import pytesseract
+import numpy as np
+
+filename = 'canada_ontario_cvo_1_1.jpg' 
+img1 = np.array(Image.open(filename))
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe' #Directory where Tesseract is installed
+text = pytesseract.image_to_string(img1)
+if not 'certifies that' in text :
+    text = text.replace("Ontario","Ontario certifies that" )
+    
+#print(text)
+hhh =text.split("that",1)[1].split("having",1)[0]
+ddd = hhh.splitlines()
+while '' in ddd:
+    ddd.remove('')
+for tx in ddd :
+           
+    if len(tx)<2:
+        ddd.remove(tx)
+print('Name: ', ddd[0])
+print('Address: ', ddd[1])
+
+
